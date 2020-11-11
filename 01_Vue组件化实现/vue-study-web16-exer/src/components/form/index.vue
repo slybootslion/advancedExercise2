d97c269ceb74cb05d744104be5058d8697c0689d
slybootslion <slybootslion@vip.qq.com>
2020-11-11 19:08:40 +0800

    2020-11-11 19:08:40

---
+++ 01_Vue组件化实现/vue-study-web16-exer/src/components/form/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- 1:23:11 -->
+    <KInput v-model="userInfo.username"></KInput>
+  </div>
+</template>
+
+<script>
+import KInput from '@/components/form/KInput.vue'
+  export default {
+    data() {
+      return {
+        userInfo: {
+          username: '',
+          password: ''
+        },
+        rules: {
+          username: [{required: true, message: '输入用户名'}],
+          password: [{required: true, message: '输入密码'}]
+        }
+      }
+    },
+    components: {
+      KInput,
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
