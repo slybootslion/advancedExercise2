a1d0640873abb9813313d06b8acbf29b1d9078fd
slybootslion <slybootslion@vip.qq.com>
2020-12-26 13:59:34 +0800

    2020-12-26 13:59:34

--- 02_React_l1/lg-web-music/src/App.js
+++ 02_React_l1/lg-web-music/src/App.js
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { memo, Suspense } from 'react'
 import { renderRoutes } from 'react-router-config'
 import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux'
@@ -17,7 +17,9 @@ export default memo(function App () {
     <Provider store={store}>
       <HashRouter>
         <LuAppHeader/>
-        {renderRoutes(routes)}
+          <Suspense fallback={<span>loading...</span>}>
+            {renderRoutes(routes)}
+          </Suspense>
         <LuAppFooter/>
         <AppPlayerBar/>
       </HashRouter>
--- 02_React_l1/lg-web-music/src/router/index.js
+++ 02_React_l1/lg-web-music/src/router/index.js
@@ -1,5 +1,6 @@
+import React from 'react'
 import { Redirect } from 'react-router-dom'
-
+/*
 import LuDiscover from '@/pages/discover'
 import LuRecommend from '@/pages/discover/c-pages/recommend'
 import LuRanking from '@/pages/discover/c-pages/ranking'
@@ -10,6 +11,18 @@ import LuAlbum from '@/pages/discover/c-pages/album'
 import LuMine from '@/pages/mine'
 import LuFriend from '@/pages/friend'
 import LuPlayer from "../pages/player";
+*/
+
+const LuDiscover = React.lazy(() => import('@/pages/discover'))
+const LuRecommend = React.lazy(() => import('@/pages/discover/c-pages/recommend'))
+const LuRanking = React.lazy(() => import('@/pages/discover/c-pages/ranking'))
+const LuSongs = React.lazy(() => import('@/pages/discover/c-pages/songs'))
+const LuDjRadio = React.lazy(() => import('@/pages/discover/c-pages/djradio'))
+const LuArtist = React.lazy(() => import('@/pages/discover/c-pages/artist'))
+const LuAlbum = React.lazy(() => import('@/pages/discover/c-pages/album'))
+const LuMine = React.lazy(() => import('@/pages/mine'))
+const LuFriend = React.lazy(() => import('@/pages/friend'))
+const LuPlayer = React.lazy(() => import('@/pages/player'))
 
 const routes = [
   {
