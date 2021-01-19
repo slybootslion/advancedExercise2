81b0dbc8d0c756de78611a767ec27ce3a2b491dc
slybootslion <slybootslion@vip.qq.com>
2021-01-19 09:39:59 +0800

    2021-01-19 09:39:59

--- 01_Vue组件化实现/vue-study-web16-exer/src/views/About.vue
+++ 01_Vue组件化实现/vue-study-web16-exer/src/views/About.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-   <from-comp></from-comp>
+    <from-comp></from-comp>
   </div>
 </template>
 
 <script>
 import FromComp from '../components/form/index'
+
 export default {
   name: 'About',
   components: {
@@ -19,6 +20,43 @@ export default {
       }
     }
   },
+  mounted () {
+    this.test()
+  },
+  methods: {
+    test () {
+      console.log(1);
+
+      setTimeout(() => {
+        console.log(2);
+        new Promise((resolve) => {
+          console.log(3);
+          resolve();
+        }).then(() => {
+          console.log(4);
+        });
+      });
+
+      new Promise((resolve) => {
+        console.log(5);
+        resolve();
+      }).then(() => {
+        console.log(6);
+      });
+
+      setTimeout(() => {
+        console.log(7);
+        new Promise((resolve) => {
+          console.log(8);
+          resolve();
+        }).then(() => {
+          console.log(9);
+        });
+      });
+
+      console.log(10)
+    }
+  }
 }
 </script>
 
